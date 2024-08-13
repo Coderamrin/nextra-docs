d5cd645f8674bcc133ac5f4a64b721f3743af2fd
coderamrin <coderamrin@gmail.com>
2024-08-13 11:20:57 +0600

    update

---
+++ app/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="text-center text-red-600">Footer</p>
+      <p className="text-center text-red-600">Footer</p>
+      <p className="text-center text-red-600">Footer</p>
+    </div>
+  );
+};
+
+export default Footer;
